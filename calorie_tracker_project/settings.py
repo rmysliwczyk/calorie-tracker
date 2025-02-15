78cabe797b056fe03780dfd75028c642f39319c4
Rafał Myśliwczyk <mysliwczykrafal@gmail.com>
2025-02-15 14:35:00 +0100

    Updated STATIC_URL and STATIC_ROOT not to break compatibility with deployed system.

--- calorie_tracker_project/settings.py
+++ calorie_tracker_project/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = 'calorie_tracker/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
