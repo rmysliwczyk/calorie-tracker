4d3446dfd23654235e2508764b7adfac66651eb8
Rafał Myśliwczyk <mysliwczykrafal@gmail.com>
2025-02-23 13:54:51 +0000

    Modified settings.py for deployment

--- calorie_tracker_project/settings.py
+++ calorie_tracker_project/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!fpl1q$w@5e+yxo#r^x+ki4ivcvrl^)e$azu^eq251+ypj3oew'
+with open('secret_key.txt', 'r') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
