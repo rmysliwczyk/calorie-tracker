f0f23d09b02d9cb3762b0d027a097fc0766c4324
Rafał Myśliwczyk <mysliwczykrafal@gmail.com>
2025-03-01 00:45:32 +0100

    Prepared package.json, installed typescript and react.

---
+++ calorie_tracker/static/calorie_tracker/react/components/info_card.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function InfoCard(title: string, content: string) {
+    return (
+        <div className="react-info-card">
+            <h5>{title}</h5>
+            <p>{content}</p>
+        </div>
+    )
+}
